26fd8fe9df808b19735cd6446fd2278716741d1a
Isma3l-19 <ismaelnyambu.99@gmail.com>
2025-06-14 10:00:02 +0300

    Managed to create a model and upload an image

--- geeks/gfg_site_app/models.py
+++ geeks/gfg_site_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class GeeksModel(models.Model):
+    # field names
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    image = models.ImageField(upload_to="images/%Y/%m/%d")
+
+
+    def __str__(self):
+        return self.title
